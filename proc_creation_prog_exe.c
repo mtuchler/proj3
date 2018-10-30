9d4efd7e0327cdbc246a4870290542d263bf5b49
Abhishek Kosuri <kosuri@rockhopper-05.cs.wisc.edu>
2018-10-30 18:18:04 -0500

    forked

---
+++ proc_creation_prog_exe.c
@@ -0,0 +1,30 @@
+#include "proc_creation_prog_exe.h"
+#include <stdio.h>
+#include <stlib.h>
+#include <sys/wait.h>
+
+//this method creates a child file from the parent file
+//and makes it run as its own once execvp is called 
+//thus terminates the process once it is finished
+void fork(){
+	pid_t  pid;
+	pid = fork();
+	char buff;
+	char imput[256];
+	
+	//checks if pid is valid or not of the child process
+	if (pid < 0){
+		printf("Error occured, could not fork\n");
+	}
+	//if the child process is found, execute it and terminate once finished
+	if(pid == 0){
+		execvp();	
+	}
+	//wait for a child process to be found
+	else{
+		wait();
+	}
+
+}
+
+
