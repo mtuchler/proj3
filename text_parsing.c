c8736b6b33787d779004db2067b99740b944eb75
Abhishek Kosuri <kosuri@rockhopper-04.cs.wisc.edu>
2018-11-01 17:34:06 -0500

    fml

--- text_parsing.c
+++ text_parsing.c
@@ -1,57 +1,63 @@
 #include "text_parsing.h"
 
+char lineBuffer[BUFF_SIZE];
+int line = 0;
 //
 FILE* openFile(){
 	file  = fopen("Makefile", "r");
-	char *lineBuffer = (char *)malloc(sizeof(char) * BUFF_SIZE);
+	int c = getc(file);
+
+	if(file == NULL){
+                printf("Error opening file\n");
+                exit(1);
+        }
+
 	fclose(file);
 }
 
 //function to parse the target of each line
 //inputs buffer to an empty array and pointer to an open file
-int parseTargets(char* name, FILE* file){
+//returns line number 
+int  parseTargets(char* name, FILE* file){
 
-	char* token = strtok(lineBuffer, ":");
-	
-	if(file == NULL){
-		printf("Error opening file\n");
-		exit(1);
-	}
+	int strLen = strlen(file);
 
-	else if(feof(file)){
+	if(feof(file)){
 		return -1;
 	}
 
 	else{
-		while(token!=NULL){
-			printf("%s\n", token);
-			token = strtok(NULL, "/t");
+		while(scanf("%d" , &c) != EOF){
+			for(int i = 0; i<strlen; i++){
+				
+			}
 			//handling target
 		
 		}
 	}	
-	return 0:
+	return line;
 }
 
 //function to parse the dependencies on each line
 //inputs buffer to an empty array
-int parseDependencies(char* name, FILE* file){
+//returns an array of strings each one being the name
+//of the dependencies
+int parseDependencies(int line, FILE* file){
        
-       	char* token = strtok(lineBuffer, "\t");
+	char* array[];
 
-        if(file == NULL){
-                printf("Error opening file\n");
-                exit(1);
-        }
-
-	else if(feof(file)){
+	if(feof(file)){
                 return -1;
 	}
 
         else{
                 while(token!=NULL){
+			for(int i = 0; i<sizeOf(Array);i++){
+				
+			
+			}
                         printf("%s\n", token);
-                        token = strtok(NULL, "");
+                        token = strtok(NULL, ":");
                         //handling dependencies
                 }
         }
