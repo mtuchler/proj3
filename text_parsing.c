996fff8c0cfbf60fef2b0fd12ada538590e09470
Abhishek Kosuri <kosuri@rockhopper-04.cs.wisc.edu>
2018-10-31 19:29:06 -0500

    code

--- text_parsing.c
+++ text_parsing.c
@@ -2,23 +2,25 @@
 
 //parses the lines of Makefile and blah blah
 int parser(int argc, char** argv){
-    	FILE* F;
-    	char buff[1024];
+    	FILE* file;
+    	int buff = 1024;
 	int c;
+	char *lineBuffer = (char *)malloc(sizeof(char) * buffer);
+	char* token = strok(buff, ":");
+
+	file  = fopen("Makefile","r");
 	
-	F  = fopen("Makefile","r");
-	
-	if(F == NULL){
+	if(file == NULL){
 		printf("Error opening file\n");
 		exit(1);
 	}	
 	else{
-		while((c =fgetc(input_file)) != EOF){	
+		while(){	
 			if(){
 								
 			}
 		}	
 	}
-	fclose(fd);
+	fclose(file);
 
 }
