a302d3499fb2c938a86b207b606d500cdcdaf2e7
Abhishek Kosuri <kosuri@rockhopper-04.cs.wisc.edu>
2018-11-01 16:15:34 -0500

    code

--- text_parsing.c
+++ text_parsing.c
@@ -1,27 +1,59 @@
 #include "text_parsing.h"
 
-//parses the lines of Makefile and blah blah
-int parseTargets(char* target){
-    	FILE* file;
-    	int buff = 1024;
-	int c;
-	char *lineBuffer = (char *)malloc(sizeof(char) * buffer);
-	char* token = strtok(buff, ":");
+//
+FILE* openFile(){
+	file  = fopen("Makefile", "r");
+	char *lineBuffer = (char *)malloc(sizeof(char) * BUFF_SIZE);
+	fclose(file);
+}
 
-	file  = fopen(filename, "r");
+//function to parse the target of each line
+//inputs buffer to an empty array and pointer to an open file
+int parseTargets(char* name, FILE* file){
+
+	char* token = strtok(lineBuffer, ":");
 	
 	if(file == NULL){
 		printf("Error opening file\n");
 		exit(1);
-	}	
+	}
+
+	else if(feof(file)){
+		return -1;
+	}
+
 	else{
 		while(token!=NULL){
 			printf("%s\n", token);
-			token = strtok(NULL, ":");
-			//handling target and dependencies
-		}	
+			token = strtok(NULL, "/t");
+			//handling target
+		
+		}
+	}	
+	return 0:
+}
 
+//function to parse the dependencies on each line
+//inputs buffer to an empty array
+int parseDependencies(char* name, FILE* file){
+       
+       	char* token = strtok(lineBuffer, "\t");
+
+        if(file == NULL){
+                printf("Error opening file\n");
+                exit(1);
+        }
+
+	else if(feof(file)){
+                return -1;
 	}
-	fclose(file);
 
+        else{
+                while(token!=NULL){
+                        printf("%s\n", token);
+                        token = strtok(NULL, "");
+                        //handling dependencies
+                }
+        }
+	return NULL;
 }
