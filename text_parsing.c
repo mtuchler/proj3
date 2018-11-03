e023fe83be5ea48343e1ef75dadd68c5dcc64b87
Abhishek Kosuri <kosuri@rockhopper-05.cs.wisc.edu>
2018-11-03 11:24:28 -0500

    restart

--- text_parsing.c
+++ text_parsing.c
@@ -3,7 +3,8 @@
 char* lineBuffer[BUFF_SIZE];
 char line[BUFF_SIZE];
 int lineNum;
-//
+
+//file handling method
 FILE* openFile(){
 	FILE* file  = fopen("filename", "r");
 
@@ -15,6 +16,7 @@ FILE* openFile(){
 	return file;
 }
 
+//method to close the file once it has been openend
 void closeFile(FILE* f) {
 	fclose(f);
 	return;
@@ -26,28 +28,27 @@ void closeFile(FILE* f) {
 int parseTargets(char* name, FILE* file){
 
 	// dummy text for fewer errors
-	if (file == NULL && name == NULL) { return 1; }	
-/*
-	int strlen = strlen(name);
-
-	if(feof(file)){
-		return -1;
+	if (file == NULL && name == NULL){
+		return 1; 
 	}
 
 	else{
-		while(fgets(line, sizeof line, file)!=NULL){
-			if(lineBuffer[i] != ':'){
-				for(int i = 0; i<strlen; i++){
-					lineBuffer[i] = line;
-					lineNum++;
-					name = lineBuffer[0];	
-				}
+		char c;
+		while((c != EOF)){
+
+			scanf("%c", &c);
+			//read til you encounter a colon character
+			if(c == ':'){
+				
 			}
-			//handling target
-			//first element of array is the target
+			else if(c == "\n"){
+				
+			}
+			else{
+					
+			}
 		}
 	}	
-*/
 	return 0;
 }
 
@@ -56,49 +57,17 @@ int parseTargets(char* name, FILE* file){
 //returns an array of strings each one being the name
 //of the dependencies
 char** parseDependencies(int lineNum, FILE* file){
-
-// dummy line plz delete
-if (lineNum == 0 && file == NULL) { printf("\n"); }
- /*	
-	char** array;
-
-	if(feof(file)){
-                return NULL;
-	}
-        else{
-		if(*lineBuffer[0] == '\n') {
-        		for(int j = 1; j < BUFF_SIZE; j++){
-				//dependencies start after the comma
-				//put dependencies into a seperate array
-				array[j];
-			}
-		}
-		else if(*lineBuffer[0] == '\t') {
-		
-		}
-                        //handling dependencies
-        }
-	return array;
-*/ return NULL;
+	return NULL;
 }
 
 //fucniton parses the command line 
 int parseCommandLine(int argc, char **argv, FILE* file){
 
-	if (argc == 0 && argv == NULL && file == NULL) { return 1; }
-/*
-	if(feof(file)){
-                return -1;
-        }
-	
-	else{
-		while(fgets(line, sizeof line, file) == '\t'){
-			if(1){
-				
-			}
-		}
+	if (argc == 0 && argv == NULL && file == NULL){
+		return 1;
 	}
+	else{
 
-*/
+	}
 	return 0;
 }
